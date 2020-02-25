93dd45d7916573efc90d8106df4bc48e21e71828
qiannyqian <liang.huiqian@gmail.com>
2020-02-25 16:13:22 +0800

    Implemented gradient links, changed header colour

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `teal`,
+      background: `lavenderblush`,
       marginBottom: `1.45rem`,
     }}
   >
